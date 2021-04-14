edb77f6938810342ad2e57b90d1901ecb4c0c4c3
Chase Parjani <JapanHasRice@gmail.com>
2021-04-14 16:48:44 -0400

    Adds recipe to convert item versions into placeable block versions without losing previous relative compass data. Fixes creative inventory issue with blocks. Removes item versions from creative inventory. Fixes FP Hand Rotation of blocks. Marks item versions as DEPRECATED with instructions on how to update.

--- src/block/BlockRelativeCompass.cs
+++ src/block/BlockRelativeCompass.cs
@@ -2,11 +2,29 @@ using System;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.MathTools;
+using Vintagestory.API.Server;
 
 namespace Compass {
   class BlockRelativeCompass : BlockCompass {
+    private static AssetLocation update = new AssetLocation("compass:recipes/grid/update.json");
     public override float GetNeedleAngleRadians(BlockPos fromPos, ItemStack compass) {
       return GetAngleRadians(fromPos, GetCompassCraftedPos(compass));
+    }
+
+    public override void OnCreatedByCrafting(ItemSlot[] allInputslots, ItemSlot outputSlot, GridRecipe byRecipe) {
+      if (byRecipe.Name.Equals(update) && this.api.Side == EnumAppSide.Server) {
+        foreach (var slot in allInputslots) {
+          if (!slot.Empty) {
+            var x = slot.Itemstack?.Attributes.GetInt("compass-target-x");
+            var z = slot.Itemstack?.Attributes.GetInt("compass-target-z");
+            if (x == null || z == null) return;
+            var targetPos = new BlockPos((int)x, 0, (int)z);
+            var y = ((ICoreServerAPI)api).World.BlockAccessor.GetTerrainMapheightAt(targetPos);
+            targetPos.Y = y;
+            SetCompassCraftedPos(outputSlot.Itemstack, targetPos);
+          }
+        }
+      }
     }
   }
 }
